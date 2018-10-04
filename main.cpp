4c922963f94071639c8831e844503c992cdab69f
Russell Wong <russellwmy@gmail.com>
2018-10-04 21:13:49 +0800

    Updated readme

--- main.cpp
+++ main.cpp
@@ -8,20 +8,20 @@ using namespace std;
 
 int main(int argc, const char *argv[])
 {
-    cout << "Day - 1: Tower of Hanoi" << endl;
+    cout << "Day 1: Tower of Hanoi" << endl;
     hanoi(3, 'A', 'B', 'C');
 
-    cout << "Day - 2: Matrix Chain Multiplication" << endl;
+    cout << "Day 2: Matrix Chain Multiplication" << endl;
     int arr[] = {10, 20, 30, 40};
     int n = sizeof(arr) / sizeof(arr[0]);
     cout << "Minimum number of multiplications is " << matrixChainOrder(arr, n) << endl;
 
-    cout << "Day - 3: Next Permutation" << endl;
+    cout << "Day 3: Next Permutation" << endl;
     string s = "FDAE";
     bool val = next_permutation(s);
     cout << "The Result is " << s << endl;
 
-    cout << "Day - 4: Count Bits" << endl;
+    cout << "Day 4: Count Bits" << endl;
     int bc = count_bits(99);
     cout << "99 has " << bc << " bits." << endl;
 }
